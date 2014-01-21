7533c2ac18411489b2c198788b18616484277e15
Bashir Eghbali <bashir@gmail.com>
2014-01-21 03:48:35 -0800

    added nonce to mail api

--- lib/mailboto.rb
+++ lib/mailboto.rb
@@ -2,11 +2,11 @@ module Mailboto
 
   class Email
     include HTTParty
-
+    NONCE = '821222a91eb75f136ea40899'
     base_uri 'http://stow.mailbsend.com/createInbox.php'
 
     def create(name)
-      self.class.post("?username=#{name}")
+      self.class.post("?username=#{name}&nonce=#{NONCE}")
     end
   end
 end
