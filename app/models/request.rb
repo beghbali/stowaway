d8e30ca6b8feb50cd1000e3b6c21bd66934189b7
Bashir Eghbali <bashir@gmail.com>
2014-02-19 23:41:14 -0800

    mend

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,10 +1,6 @@
 class Request < ActiveRecord::Base
   STATUSES = %w(outstanding matched fulfilled cancelled timedout)
-<<<<<<< HEAD
   DEVICE_TYPES = %w(ios android)
   validates :status, inclusion: { in: STATUSES }
   validates :device_type, inclusion: { in: DEVICE_TYPES }
-=======
-  validates :status, inclusion: { in: STATUSES }
->>>>>>> parent of dc396ca... Merge pull request #4 from TrexMarketing/feature/65420424/match-requests
 end
