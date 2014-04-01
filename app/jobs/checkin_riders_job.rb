6a4fdd1da01c65f8647ddd209cdc861c096fa5f6
Bashir Eghbali <bashir@gmail.com>
2014-04-01 01:32:29 -0700

    added pusher auth method to background process

--- app/jobs/checkin_riders_job.rb
+++ app/jobs/checkin_riders_job.rb
@@ -1,16 +1,16 @@
 require 'pusher-client'
 
 class CheckinRidersJob
+  include Httparty
+
   @queue = :checkin_queue
 
   def self.perform(ride_id)
     ride = Ride.find(ride_id)
     raise "ride not found: #{ride_id}" if ride.nil?
 
-    socket = PusherClient::Socket.new(ENV['PUSHER_KEY'], secret: ENV['PUSHER_SECRET'], encrypted: true)
-    Rails.logger.debug('got socket')
+    socket = PusherClient::Socket.new(ENV['PUSHER_KEY'], secret: ENV['PUSHER_SECRET'], encrypted: true, private_auth_method: :authenticate)
     socket.subscribe(ride.location_channel_name, ENV['PUSHER_SERVER_USER_ID'])
-    Rails.logger.debug("subscribed to #{ride.location_channel_name} with #{ENV['PUSHER_SERVER_USER_ID']}")
 
     socket[ride.location_channel_name].bind('client-location-update') do |data|
       Rails.logger.debug "DATA:#{data}"
@@ -19,5 +19,9 @@ class CheckinRidersJob
       socket.disconnect if ride.closed?
     end
     socket.connect
+  end
+
+  def self.authenticate(socket_id, channel)
+    post "#{ENV[SERVER_ADDRESS]}/pusher/#{ENV['PUSHER_SERVER_USER_ID']}/auth", {channel_name: channel, socket_id: socket_id}
   end
 end
