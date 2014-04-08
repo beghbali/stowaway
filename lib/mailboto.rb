61049e9dcac17d3953fe5bb1a0697f6eaf8b6a03
Bashir Eghbali <bashir@gmail.com>
2014-04-07 23:47:16 -0700

    url encode emails for getstowaway account creation

--- lib/mailboto.rb
+++ lib/mailboto.rb
@@ -6,7 +6,7 @@ module Mailboto
     base_uri 'http://getstowaway.com/createInbox.php'
 
     def create(name, existing_email)
-      response = self.class.post("?" + mailboto_params(name, existing_email), options: { headers: { 'ContentType' => 'application/json' } } )
+      response = self.class.post("?" + URI.encode(mailboto_params(name, existing_email)), options: { headers: { 'ContentType' => 'application/json' } } )
       json = JSON.parse(response)
       [json['email'], json['password']]
     end
