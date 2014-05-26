7d222bba0fcd90ff7994453f600987c90047c879
Bashir Eghbali <bashir@gmail.com>
2014-05-26 13:35:27 -0700

    added resque logger

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,1 +1,3 @@
 Resque.redis = Redis.new(host: ENV['REDIS_HOST'] || "localhost", port: ENV['REDIS_PORT'] || 6379, :password => ENV['REDIS_PASSWORD'])
+Resque.logger = Logger.new
+Resque.logger.level = Logger::DEBUG
