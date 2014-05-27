c1457886b9ac7e93bc808614d19dafaec74f6706
Bashir Eghbali <bashir@gmail.com>
2014-05-27 08:26:12 -0700

    fix notifications

--- app/jobs/notification_job.rb
+++ app/jobs/notification_job.rb
@@ -3,10 +3,10 @@ class NotificationJob
   @queue = :notifications
   @retry_exceptions = [Timeout::Error, Errno::ECONNREFUSED, Errno::ECONNRESET]
 
-  def self.perform(klass, id, options)
-    notifiable = klass.constantize.where(id: id).first
+  def self.perform(klass, public_id, options)
+    notifiable = klass.constantize.where(public_id: public_id).first
 
-    Resque.logger.info "About to notify: #{notifiable.try(:public_id)}"
+    Rails.logger.info "About to notify: #{notifiable.try(:public_id)}"
 
     unless notifiable.nil?
       notifiable.notify!(options)
--- lib/notify.rb
+++ lib/notify.rb
@@ -43,14 +43,14 @@ module Notify
 
     def notify!(options = {})
       return if cannot_be_notified?
-      Resque.logger.info "NOTIFICATION: #{options} TO: #{self.device_token}"
+      Rails.logger.info "NOTIFICATION: #{options} TO: #{self.device_token}"
       if device_type.to_sym == :ios
         APNS.send_notification(self.device_token, options)
       else
         # GCM.send_notification(self.device_token, options[:other])
       end
     rescue Exception => e
-      Resque.logger.error("NOTIFICATION: FAILED #{e.message}")
+      Rails.logger.error("NOTIFICATION: FAILED #{e.message}")
     end
   end
 end
