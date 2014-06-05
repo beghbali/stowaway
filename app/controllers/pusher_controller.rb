ee5dee06183d9f8c4c3ad373096bab88be05f1a2
Bashir Eghbali <bashir@gmail.com>
2014-06-05 00:20:07 -0700

    mend

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -3,6 +3,7 @@ class PusherController < ApplicationController
 
   def auth
     if current_user
+      Rails.logger.info "AUTHENTICATING: #{params}"
       response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
       render :json => response
     else
