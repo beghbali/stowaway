2af63887539f456d695445d268ed9b80d94b076e
Bashir Eghbali <bashir@gmail.com>
2014-06-05 00:17:36 -0700

    mend

--- lib/notify.rb
+++ lib/notify.rb
@@ -45,9 +45,9 @@ module Notify
       return if cannot_be_notified?
       options = options.with_indifferent_access
 
-      Rails.logger.info "NOTIFICATION: #{options} TO: #{self.try(:device_token)}"
+      Rails.logger.info "NOTIFICATION: #{options} TO: #{self.try(:device_token)}, #{self.device_type.to_sym == :ios}, #{self.device_type}"
 
-      if device_type.to_sym == :ios
+      if self.device_type.to_sym == :ios
         Rails.logger.info "APNS: #{APNS::Notification.new(self.device_token, options).packaged_message}"
         APNS.send_notification(self.device_token, options)
       else
