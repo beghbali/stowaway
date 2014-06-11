84653447a0401b2b62d490e2907ac61e6304f5cb
Bashir Eghbali <bashir@gmail.com>
2014-06-11 00:00:23 -0700

    cleaned up closeride

--- app/jobs/close_ride_job.rb
+++ app/jobs/close_ride_job.rb
@@ -4,9 +4,10 @@ class CloseRideJob
   @retry_exceptions = [Timeout::Error, Errno::ECONNREFUSED, Errno::ECONNRESET]
 
   def self.perform(ride_id)
-    ride = Ride.find(ride_id)
-    raise "ride not found: #{ride_id}" if ride.nil?
-    Rails.logger.info "[AUTOCHECKIN] closing ride #{ride_id}"
-    ride.close
+    ride = Ride.where(id: ride_id).first
+    if ride.present?
+      Rails.logger.info "[AUTOCHECKIN] closing ride #{ride_id}"
+      ride.close
+    end
   end
 end
