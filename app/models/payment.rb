3f363d5e2280058b6cd507d8b590f9623773c1c8
Bashir Eghbali <bashir@gmail.com>
2014-06-27 02:54:58 -0700

    correct billed to to be the email

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,7 +5,7 @@ class Payment < ActiveRecord::Base
   before_create :generate_receipt
 
   def generate_receipt
-    self.build_receipt(generated_by: 'Stowaway', billed_to: request.rider.full_name, user_id: request.rider.id,
+    self.build_receipt(generated_by: 'Stowaway', billed_to: request.rider.email, user_id: request.rider.id,
       ride_requested_at: request.created_at, pickup_location: request.ride.suggested_pickup_address,
       dropoff_location: request.ride.suggested_dropoff_address, total_amount: self.amount, other_amount: self.fee,
       other_description: I18n.t('models.payment.fee'), distance: request.ride.distance,
