cc63b01ca7cb297db7cfdb06e32f5486cdacd67a
Bashir Eghbali <bashir@gmail.com>
2014-06-27 04:03:16 -0700

    mend

--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -8,7 +8,7 @@ class ReceiptMailer < ActionMailer::Base
          subject: I18n.t('mailers.receipt_mailer.captain_ride_receipt.subject',
           weekday: @receipt.request.ride.created_at.strftime("%A"),
           timeofday: @receipt.request.ride.created_at.to_datetime.time_of_day,
-          savings: "#{number_with_percision(@receipt.savings * 100, percision: 2)}%")
+          savings: "#{number_with_percision(@receipt.savings * 100, percision: 2)}%"))
   end
 
   def stowaway_ride_receipt(receipt_id)
