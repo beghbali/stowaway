f894d181c4f4a20cfe532cd1934c94b03fd6bb00
Bashir Eghbali <bashir@gmail.com>
2014-06-27 03:58:18 -0700

    fix styles + subject + conditionally show lone rider info, only show credits when applicable

--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -8,7 +8,7 @@ class ReceiptMailer < ActionMailer::Base
          subject: I18n.t('mailers.receipt_mailer.captain_ride_receipt.subject',
           weekday: @receipt.request.ride.created_at.strftime("%A"),
           timeofday: @receipt.request.ride.created_at.to_datetime.time_of_day,
-          savings: "#{@receipt.savings * 100}%"))
+          savings: "#{number_with_percision(@receipt.savings * 100, percision: 2)}%")
   end
 
   def stowaway_ride_receipt(receipt_id)
@@ -17,6 +17,6 @@ class ReceiptMailer < ActionMailer::Base
          subject: I18n.t('mailers.receipt_mailer.stowaway_ride_receipt.subject',
           weekday: @receipt.request.ride.created_at.strftime("%A"),
           timeofday: @receipt.request.ride.created_at.to_datetime.time_of_day,
-          savings: "#{@receipt.savings * 100}%"))
+          savings: "#{number_with_percision(@receipt.savings * 100, percision: 2)}%"))
   end
 end
