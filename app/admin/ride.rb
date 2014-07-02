333086dd9472f01d1a6d874dda7d86f0524f7f43
Bashir Eghbali <bashir@gmail.com>
2014-07-01 23:42:21 -0700

    find_by_public_id in active admin and also make it include requests in ride view

--- app/admin/ride.rb
+++ app/admin/ride.rb
@@ -1,6 +1,13 @@
 ActiveAdmin.register Ride do
 
-  
+
+  controller do
+    defaults :finder => :find_by_public_id
+
+    def scoped_collection
+      Ride.includes(:requests)
+    end
+  end
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -13,5 +20,5 @@ ActiveAdmin.register Ride do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+
 end
